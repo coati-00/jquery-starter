710e96ce1e68f5636f83f252e45ef11a255a984f
Cassandra Dunlop <cdulop@columbia.com>
2014-12-24 19:37:41 -0500

    assignment done

--- js/app.js
+++ js/app.js
@@ -27,6 +27,26 @@ $(document).ready(function () {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+
+  /* The assignment refers to the jQuery page on keydown,
+  there it says if it is meant to be globally accessible
+  to just attach it to the document, however it also says
+  something like only the element selected will get the event handler*/
+  $(document).keydown(function(event){
+    //console.log("keydown");
+    //console.log(event.which);
+    if ( event.which === 88 ){
+      $('.ryu-still').hide();
+      $('.ryu-cool').show();
+    }
+  }).keyup(function(event){
+    //console.log("keyup");
+    //console.log(event.which);
+    if (event.which === 88) {
+      $('.ryu-still').show();
+      $('.ryu-cool').hide();
+    }
+  });
 });
 
 function playHadouken () {
